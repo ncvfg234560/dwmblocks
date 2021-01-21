2b02eac63a538f4031f423c102630c42965d6215
Bryce Vandegrift <30876423+ncvfg234560@users.noreply.github.com>
2021-01-20 22:11:43 -0500

    Add files via upload

---
+++ blocks.h
@@ -0,0 +1,18 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+        {"  ", "./scripts/crypto",                                             3600,           0},
+
+	{"❯ ", "uname -s",							0,		0},
+
+	{"", "./scripts/vol",							10,		0},
+
+	{"", "./scripts/net",							10,		0},
+
+	{"🕒 ", "date '+%a %b %d %I:%M%p'",					60,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
